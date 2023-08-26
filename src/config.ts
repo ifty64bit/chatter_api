4616c0c57ae6ad3c132637182fedf77225dbb333
ifty64bit <ifty64bit@gmail.com>
2023-08-27 00:14:38 +0600

    added firebase cred

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,7 @@ export default () => ({
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND || 10,
     JWT_SECRET: process.env.JWT_SECRET || "secret",
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_x509_cert_url: process.env.client_x509_cert_url,
 })
---
+++ src/service-account.ts
@@ -0,0 +1,17 @@
+import config from "./config"
+
+export default {
+    type: "service_account",
+    project_id: "chatter-fd06e",
+    private_key_id: config().private_key_id,
+    private_key:
+        config().private_key?.replace(/\\n/g, "\n") || "private_key_not_found",
+    client_email:
+        "firebase-adminsdk-gaerl@chatter-fd06e.iam.gserviceaccount.com",
+    client_id: "104820527627158540354",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: config().client_x509_cert_url,
+    universe_domain: "googleapis.com",
+}
