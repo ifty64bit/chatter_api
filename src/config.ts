98f0a4845b2351a463b34a1216c8ec8a9766bac5
Ifty <ifty64bit@gmail.com>
2024-09-25 02:36:38 +0600

    update CI/CD

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export default () => ({
     JWT_SECRET: process.env.JWT_SECRET || "secret",
     GOOGLE_APPLICATION_CREDENTIALS: process.env
         .GOOGLE_APPLICATION_CREDENTIALS as string,
-    private_key_id: process.env.private_key_id as string,
-    private_key: process.env.private_key as string,
-    client_x509_cert_url: process.env.client_x509_cert_url as string,
+    private_key_id: process.env.PRIVATE_KEY_ID as string,
+    private_key: process.env.PRIVATE_KEY as string,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL as string,
 })
